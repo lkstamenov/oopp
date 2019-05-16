4a906ea295ed077570cf433523a6ea7e93116866
Lachezar Stamenov <lkstamenov@yahoo.com>
2019-05-16 11:57:07 +0000

    employee.py

---
+++ employee.py
@@ -0,0 +1,21 @@
+# Object Oriented Programming in Python
+
+class Employee:
+
+  def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+
+
+emp_1 = Employee('Corey', 'Schafer', 5000)
+emp_2 = Employee('Test', 'User', 6000)
+
+print(emp_1)
+print(emp_2)
+
+print(emp_1.email)
+print(emp_2.email)
+
+
